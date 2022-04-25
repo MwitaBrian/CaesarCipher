9a628463575adfb93768cdf5e9903d26ad4ef063
MwitaBrian <brianmwita13@gmail.com>
2022-04-25 14:08:44 +0300

    Remove alphabet Array code

--- src/main/java/models/Cipher.java
+++ src/main/java/models/Cipher.java
@@ -2,7 +2,6 @@ package models;
 public class Cipher {
 
     int shift = 7;
-    Character[] alphabet = {'A', 'B', 'C','D', 'E', 'F','G', 'H', 'I','J', 'K', 'L','M', 'N', 'O','P', 'Q', 'R','S', 'T', 'U','V', 'W', 'X','Y', 'Z'};
     public String encrypt(String userInput){
         String EncryptedText = "";
 
