c151267f226866df801b2f339e414a9f3aec6fa0
MwitaBrian <brianmwita13@gmail.com>
2022-04-25 15:17:41 +0300

    Receive shift key from user in command line

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,12 +5,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Scanner shiftKey = new Scanner(System.in);
         Cipher newCipher = new Cipher();
 
         System.out.println("Welcome to Caesar Cipher Program.");
         System.out.println("Please enter the Text you would like to Encrypt:");
-
         String userInput = input.nextLine();
 
+        System.out.println("Enter shift Key: (1-25)");
+        int shift = shiftKey.nextInt();
     }
 }
