32279929d7dda2e7cd112928383f63b8234528b3
MwitaBrian <brianmwita13@gmail.com>
2022-04-25 15:09:48 +0300

    Use Scanner to receive userInput in terminal

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,16 @@
+import models.Cipher;
+
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Cipher newCipher = new Cipher();
+
+        System.out.println("Welcome to Caesar Cipher Program.");
+        System.out.println("Please enter the Text you would like to Encrypt:");
+
+        String userInput = input.nextLine();
+
+    }
 }
